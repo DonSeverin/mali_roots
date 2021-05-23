3e43079ccec7093b6cd989f41620ae47387ac747
Don Severin <dseverin10@hotmail.com>
2021-05-23 06:38:02 +0200

    Update settings.py

--- m_roots/m_roots/settings.py
+++ m_roots/m_roots/settings.py
@@ -55,6 +55,11 @@ MIDDLEWARE = [
 
 ]
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'm_roots.urls'
 
 TEMPLATES = [
